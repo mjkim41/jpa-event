17cdbcb435fba029590ca701c72f07e97ed07dbe
futuredeveloper41 <mj10283@naver.com>
2025-02-21 09:27:16 +0900

    # 이메일 인증코드 발송 기본설정

--- src/main/java/com/study/event/EventApplication.java
+++ src/main/java/com/study/event/EventApplication.java
@@ -1,5 +1,6 @@
 package com.study.event;
 
+import com.study.event.config.DotEnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EventApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventApplication.class, args);
+
+		SpringApplication app = new SpringApplication(EventApplication.class);
+		app.addInitializers(new DotEnvConfig());
+		app.run(args);
+
 	}
 
 }
